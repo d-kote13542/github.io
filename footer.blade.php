80218eb17ae49d297cf666a157d1c76b662cc194
d-kote13542 <92093176+d-kote13542@users.noreply.github.com>
2021-12-15 01:05:23 +0100

    Add files via upload

---
+++ footer.blade.php
@@ -0,0 +1,58 @@
+<footer>
+    <div class="footer-block">
+
+        <div class="footer-block_map">
+            <div class="footer-top_contact"></div>
+            <div class="footer-top_map"></div>
+        </div>
+
+        <div class="footer-navigation">
+            <div class="footer-navigation_left-block">
+                <div class="footer-navigation_left-block_logo">
+                    <img src="{{asset('assets/img/logo/logo-white.png')}}">
+                </div>
+
+                <div class="footer-navigation_left-block_accounts">
+                    <img src="{{ asset('assets/img/icons/facebook.png') }}">
+                    <img src="{{ asset('assets/img/icons/instagram-white.png') }}">
+                    <span>© face lab</span>
+                </div>
+            </div>
+
+            <div class="footer-navigation_block">
+                <div>
+                    <h3>ИНФОРМАЦИЯ</h3>
+                    <p><a>Специалисты</a></p>
+                    <p><a>Косметика</a></p>
+                    <p><a>До и После</a></p>
+                    <p><a>Пациенту</a></p>
+                    <p><a>оборудование</a></p>
+                    <p><a>Дицензия МОЗ</a></p>
+                </div>
+            </div>
+
+            <div class="footer-navigation_block">
+                <div>
+                    <h3>Услуги и прайс</h3>
+                    <p><a>Классическая косметология</a></p>
+                    <p><a>Аппаратная косметология лица и тела</a></p>
+                    <p><a>Инъекционная косметология</a></p>
+                    <p><a>Лазерное лечение и эпиляция</a></p>
+                    <p><a>Обучение врачей и специалистов</a></p>
+                    <p><a>Прайс</a></p>
+                </div>
+            </div>
+
+            <div class="footer-navigation_right-block">
+                <p><img src="{{ asset('assets/img/icons/call-white.png') }}">096 381 93 23</p>
+                <p><a href="#">записаться онлайн</a></p>
+            </div>
+        </div>
+
+    </div>
+</footer>
+
+<script src="{{ asset('vendor/js/slick.min.js') }}"></script>
+<script src="{{ asset('vendor/js/comment-slider.js') }}"></script>
+    </body>
+</html>
---
+++ header.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.css" type="text/css">
+        <!-- Scripts -->
+        <script src="{{ asset('vendor/js/slider-top.js') }}"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+
+
+        <title> &ndash; FaceLab</title>
+    </head>
+    <body>
+        @include('layouts.components.header.navigation')
+
